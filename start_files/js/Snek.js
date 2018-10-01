63a888737ec03d65d1b792ce61447a7aa302fe6e
Peter Gyory <ptgyory@gmail.com>
2018-10-01 09:51:05 -0600

    Add start files for 1st workshop

---
+++ start_files/js/Snek.js
@@ -0,0 +1,4 @@
+class Snek {
+  constructor() {
+  }
+}
---
+++ start_files/js/constants.js
@@ -0,0 +1,23 @@
+const KEYS = {
+  // Arrow keys
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+
+  // W, A, S, D keys
+  W: 87,
+  A: 65,
+  S: 83,
+  D: 68,
+
+  // Other keys
+  SPACE: 32,
+};
+
+const DIRECTIONS = {
+  LEFT: 'LEFT',
+  UP: 'UP',
+  RIGHT: 'RIGHT',
+  DOWN: 'DOWN',
+}
